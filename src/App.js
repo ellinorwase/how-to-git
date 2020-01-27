b7bee15e0a645d83e14dd4f58a5b4bdd0897b035
Frida Schoultz <fridaschoultz@hotmail.com>
2020-01-27 14:48:16 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Menu from './components/menu'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div className="App">
       <Menu/>
+      <Footer />
     </div>
   );
 }
