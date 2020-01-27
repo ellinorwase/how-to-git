500269686cf1531be6430c6e3973b3b9cc893419
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-27 11:26:32 +0100

    menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import '../css/menu.css'
+
+
 //rce
 export class menu extends Component {
     render() {
         return (
-            <div>
+            <div className="menu">
                 <ul>
                     <li><a href="google.com">Start</a></li>
                     <li><a href="./commands" >Kommandon</a></li>
-                    <li><a href="./conflicts" >konflikter</a></li>
+                    <li><a href="./conflicts" >Konflikter</a></li>
                     <li><a href="">Quiz</a></li>
                     
                 </ul>
