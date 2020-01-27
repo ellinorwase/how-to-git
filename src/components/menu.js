c4426dc537eb156fb2cc79024ad735ac59bc49f6
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-27 10:47:09 +0100

    menu

---
+++ src/components/menu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+//rce
+export class menu extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><a>Start</a></li>
+                    <li><a>Kommandon</a></li>
+                    <li><a>Quiz</a></li>
+                </ul>
+                
+            </div>
+        )
+    }
+}
+
+export default menu
