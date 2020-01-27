8635166732ea0b52e409bf919684fe4d71d173f5
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-27 14:39:53 +0100

    start-sida

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Menu from './components/menu'
+import Start from './components/start'
 
 function App() {
   return (
     <div className="App">
+
+        
       <Menu/>
+      <Start/>
     </div>
   );
 }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react'
-import '../css/menu.css'
-
+import React, { Component } from "react";
+import "../css/menu.css";
 
 //rce
 export class menu extends Component {
-    render() {
-        return (
-            <div className="menu">
-                <ul>
-                    <li><a href="google.com">Start</a></li>
-                    <li><a href="./commands" >Kommandon</a></li>
-                    <li><a href="./conflicts" >Konflikter</a></li>
-                    <li><a href="">Quiz</a></li>
-                    
-                </ul>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="menu">
+        <ul>
+          <li>
+            <a href="google.com">Start</a>
+          </li>
+          <li>
+            <a href="./commands">Kommandon</a>
+          </li>
+          <li>
+            <a href="./conflicts">Konflikter</a>
+          </li>
+          <li>
+            <a href="">Quiz</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default menu
+export default menu;
