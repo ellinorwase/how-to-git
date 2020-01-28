3f687bb9af0d824d3af465fe5e8afcc2644ca83f
Frida Schoultz <fridaschoultz@hotmail.com>
2020-01-28 19:41:58 +0100

    added some command boxes

--- src/components/commands.js
+++ src/components/commands.js
@@ -11,28 +11,12 @@ export class commands extends Component {
                 <hr></hr>
                 <div className='wrapper2'>
                     <div className='wrapper3'>
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
-                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
-                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
-                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
-                    voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, 
-                    consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et 
-                    dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem 
-                    llam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum 
-                    iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui 
-                    olorem eum fugiat quo voluptas nulla pariatur?"
+                    <div className='com1'>git add .</div>
+                    <div className='com2'>git commit -m 'meddelande här'</div> 
+                    <div className='com3'>git push</div>
                     </div>
                     <div className='wrapper4'>
                     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                    totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                    dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
-                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-                     est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius 
-                     odi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
-                     is nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-                      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-                       vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
                     totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
                     dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
                     sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
