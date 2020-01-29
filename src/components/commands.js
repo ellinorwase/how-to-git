af6b67ddd50d2b452f94798db96533d1484464a9
Frida Schoultz <fridaschoultz@hotmail.com>
2020-01-29 15:33:30 +0100

    worked on commands component

--- src/components/commands.js
+++ src/components/commands.js
@@ -11,22 +11,27 @@ export class commands extends Component {
                 <hr></hr>
                 <div className='wrapper2'>
                     <div className='wrapper3'>
-                    <div className='com'>git add .</div>
-                    <div className='com'>git commit -m 'meddelande här'</div> 
-                    <div className='com'>git push</div>
+                        <div className='com'>git add .</div>
+                        <div className='com'>git commit -m 'meddelande här'</div> 
+                        <div className='com'>git push</div>
+                        <div className='com'>git push</div>
+                        <div className='com'>git push</div>
+                        <div className='com'>git push</div>
                     </div>
                     <div className='wrapper4'>
-                    <p>
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                    totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                    dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
-                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-                     est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius 
-                     odi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
-                     is nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-                      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-                       vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                    </p>
+                        <div className="total-sqr">
+                            <div className="com-sqr-1"></div>
+                            <div className="com-sqr-2"></div>
+                            <div className="com-sqr-3"></div>
+                            <div className="com-sqr-4"></div>
+                            <div className='transbox'>
+                            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+                        </div>
+                        </div>
+                        {/* <div className='transbox'>
+                            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+                        </div> */}
+                    
                     </div>
 
                 </div>
