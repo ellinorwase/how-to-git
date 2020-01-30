fbb62e20943ba9869fb59a8633a071ae91dc40b1
Ellinor Wase <ellinor.wase@gmail.com>
2020-01-30 17:27:20 +0100

    commands expl

--- src/components/commandDivs.js
+++ src/components/commandDivs.js
@@ -7,44 +7,60 @@ export class commandsDiv extends Component {
 
     this.state = {
       commandsDesc: [
-        { id: "1", command: "git add .", title: "Förklaring 1" },
+        { id: "1", command: "git add .", exp: "Förklaring 1" },
         {
           id: "2",
           command: 'git commit -m "meddelande här"',
-          title: "Förklaring 2"
+          exp: "Förklaring 2"
         },
-        { id: "3", command: "git status", title: "Förklaring 3" },
-        { id: "4", command: 'git add "file"', title: "Förklaring 4" },
-        { id: "5", command: "git push", title: "Förklaring 5" },
-        { id: "6", command: "git pull", title: "Förklaring 6" },
-        { id: "7", command: "git branch", title: "Förklaring 7" },
+        { id: "3", command: "git status", exp: "Förklaring 3" },
+        { id: "4", command: 'git add "file"', exp: "Förklaring 4" },
+        { id: "5", command: "git push", exp: "Förklaring 5" },
+        { id: "6", command: "git pull", exp: "Förklaring 6" },
+        { id: "7", command: "git branch", exp: "Förklaring 7" },
         {
           id: "8",
           command: 'git checkout -b "namnet på branchen"',
-          title: "Förklaring 8"
+          exp: "Förklaring 8"
         },
         {
           id: "9",
           command: 'git checkout "branch-namn"',
-          title: "Förklaring 9"
+          exp: "Förklaring 9"
         },
-        { id: "10", command: 'git merge "branch-namn"', title: "Förklaring 10" }
+        { id: "10", command: 'git merge "branch-namn"', exp: "Förklaring 10" }
       ]
     };
   }
 
-  render() {
-    return (
-      <div>
+  displayExplanation = (data) => {
+      // this.props.getExp(data)
+      const {getExp} = this.props
+      getExp(data);
+  };
+    
+    render() {
+        return (
+            <div>
         <div className="wrapper-com2">
           <div className="wrapper-com3">
             {this.state.commandsDesc.map((commandsDesc, index) => {
               return (
-                <button className="com" key={index}>
+                <button
+                  className="com"
+                  key={index}
+                  onClick={() => this.displayExplanation(commandsDesc.exp)}
+                >
                   {commandsDesc.command}
                 </button>
               );
             })}
+            {/* <p>
+                Skriv ut här:
+            {this.state.commandsDesc.exp}
+
+            </p> */}
+
           </div>
         </div>
       </div>
