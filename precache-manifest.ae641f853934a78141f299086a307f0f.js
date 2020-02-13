f325d3e7152c1650a69908b3b4ca080a9cd67035
Ellinor Wase <ellinor.wase@gmail.com>
2020-02-13 10:43:15 +0100

    Updates

---
+++ precache-manifest.ae641f853934a78141f299086a307f0f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa64d7ba570b834b3a2d66690ad003d9",
+    "url": "/how-to-git/index.html"
+  },
+  {
+    "revision": "9203f6baf4cb476ecd89",
+    "url": "/how-to-git/static/css/main.c30139f0.chunk.css"
+  },
+  {
+    "revision": "eefc0e367c4af8b191a3",
+    "url": "/how-to-git/static/js/2.77e7b378.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/how-to-git/static/js/2.77e7b378.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9203f6baf4cb476ecd89",
+    "url": "/how-to-git/static/js/main.3b227ad2.chunk.js"
+  },
+  {
+    "revision": "9a77daf676f8183c254f",
+    "url": "/how-to-git/static/js/runtime-main.789bbcee.js"
+  },
+  {
+    "revision": "dce72d213b8075b7e707b108fbe74b68",
+    "url": "/how-to-git/static/media/bc-quiz3.dce72d21.svg"
+  },
+  {
+    "revision": "77a276126369c16e0a5598185ff3b320",
+    "url": "/how-to-git/static/media/conflict-pic.77a27612.png"
+  },
+  {
+    "revision": "2bd9713691975eda3a7868be54da0baf",
+    "url": "/how-to-git/static/media/distributed-pic.2bd97136.png"
+  },
+  {
+    "revision": "7ec8fcf9b91a511c87ece683e06d6b3c",
+    "url": "/how-to-git/static/media/dots.7ec8fcf9.svg"
+  },
+  {
+    "revision": "ccf2dc831431dbae0553ca9c6b7a683b",
+    "url": "/how-to-git/static/media/gitpic1.1.ccf2dc83.png"
+  },
+  {
+    "revision": "db766df60a11c3759879bfd0d7bc9a45",
+    "url": "/how-to-git/static/media/path1.7.db766df6.svg"
+  },
+  {
+    "revision": "3ae9593809e19afebcdb4f58f5566216",
+    "url": "/how-to-git/static/media/pic3edit.3ae95938.png"
+  }
+]);
